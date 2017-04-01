4e9523ceec8c8de20ffa71410077a8d8e5fab12b
AriiPortal <AriiPortal@sos-paris.com>
2017-04-01 20:07:24 +0200

    Add JIDBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Arii\CoreBundle\AriiCoreBundle(),
-            new Arii\UserBundle\AriiUserBundle()
+            new Arii\UserBundle\AriiUserBundle(),
+            new Arii\JIDBundle\AriiJIDBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
